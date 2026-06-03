b7bfe3deafe3eb1283b9d6225ce4baaa2d7a7019
Shaniagrg <sglearner200@gmail.com>
2026-06-02 23:24:24 -0700

    Print Linearly from 1 to n using recursion

---
+++ lesson2/print_linearly_1ton.py
@@ -0,0 +1,13 @@
+#Print Linearly from 1 to n
+def f(i,n):
+    if i > n: #base condition: print n times which is 3 for this example
+        return
+    print (i)
+    f(i+1,n)
+
+if __name__ == "__main__": 
+    n:int = 3
+    f(1,n) 
+
+#TC = O(n) funtction is called n number times
+#SC = O(n) its stores in the internal space called stack space
