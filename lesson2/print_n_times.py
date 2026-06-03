76a613e2f2570229ae905779ecb24c6025f9c78d
Shaniagrg <sglearner200@gmail.com>
2026-06-02 23:03:03 -0700

    give TC and SC

--- lesson2/print_n_times.py
+++ lesson2/print_n_times.py
@@ -9,3 +9,6 @@ def f(i,n):
 if __name__ == "__main__": 
     n:int = 3
     f(1,n) 
+    
+#TC = O(n) cuz you are calling function n number of times
+#SC = O(n) cuz even we are not using any space to store anything the computer is using internal space called "stack space" where the functions are stacked until return
