0a4436c4185e16696ff3561c0a98455469c7f487
Shaniagrg <sglearner2022@gmail.com>
2026-06-16 23:36:34 -0700

    combination sum

---
+++ lesson8/README.md
@@ -0,0 +1,9 @@
+<h3>Combination Sum</h3>
+
+<ol>
+    <li>Input: candidates = [2,3,6,7], target = 7</li>
+    <li>Output: [[2,2,3],[7]]</li>
+    <li>Explanation:</li>
+    <li>2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.</li>
+    <li>7 is a candidate, and 7 = 7.</li>
+</ol>
--- lesson8/combination_sum.py
+++ lesson8/combination_sum.py
@@ -1,40 +1,30 @@
-class Solution:
+'''
+You can use the same value multiple times to get target
+eg: input = [3,4,1,2] target 4
+Output = [4]  
+         [1,1,1,1]  
+         [1,1,2]  
+         [2,2]
+    Here you added 1 and 2 multiple time to get the targets
+'''
 
-    # Function to find all combinations recursively
-    def findCombination(self, ind, target, arr, ans, ds):
-        # Base case: if we have considered all elements in the array
-        if ind == len(arr):
-            # If the target is zero, we have found a valid combination
-            if target == 0:
-                ans.append(list(ds))  # Add the current combination to the result
-            return
+def findCombination(ind:int, target:int, ans:list[int], combination:list[int]):
+    if ind == len(nums):
+        if target == 0:
+            ans.append(list(combination))
+        return
+    
+    if nums[ind] <= target:
+            combination.append(nums[ind])  # Add the current element to the combination
+            findCombination(ind, target - nums[ind], ans, combination)  # Continue with the same index to allow repeated elements
+            combination.pop()
+    
+    findCombination(ind + 1, target, ans, combination)
+    
 
-        # Recursive case: pick the element if it's less than or equal to the target
-        if arr[ind] <= target:
-            ds.append(arr[ind])  # Add the current element to the combination
-            self.findCombination(ind, target - arr[ind], arr, ans, ds)  # Continue with the same index to allow repeated elements
-            ds.pop()  # Backtrack by removing the last added element
-
-        # Skip the current element and move to the next index
-        self.findCombination(ind + 1, target, arr, ans, ds)
-
-    # Main function to get all combinations
-    def combinationSum(self, candidates, target):
-        ans = []  # To store the result
-        ds = []  # To store a current combination
-        self.findCombination(0, target, candidates, ans, ds)  # Start the recursive search
-        return ans  # Return all valid combinations
-
-# Driver code
-if __name__ == "__main__":
-    obj = Solution()
-    v = [2, 3, 6, 7]  # Candidate numbers
-    target = 7  # Target sum
-
-    # Get all combinations
-    ans = obj.combinationSum(v, target)
-
-    # Output the combinations
-    print("Combinations are: ")
-    for combination in ans:
-        print(" ".join(map(str, combination)))  # Print each element of the combination
+if __name__ == "__main__": 
+    nums:list[int] = [2,3,6,7]
+    k:int = 7
+    answer:list[int] = []
+    print(findCombination(ind=0,target=k,ans=answer, combination=[]))
+    print(answer)
